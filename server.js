5fc3616f68e1bebde575774e1eca9738a72bbb00
Steph Perez <tephpez@gmail.com>
2023-04-11 23:09:19 -0400

    posts & queries! :3 inching through the lessons :)

--- server.js
+++ server.js
@@ -16,21 +16,21 @@ app.listen(PORT, ()=> {
 })
 
 
-app.get('/', (req, res)=> {                 // MAKING THE LANDING PAGE
-    res.send('SERVER INITIATED')
-}) 
+// app.get('/', (req, res)=> {                 // MAKING THE LANDING PAGE
+//     res.send('SERVER INITIATED')
+// }) 
 
-app.get('/users', (req, res)=> {                 // USERS
-    res.send('USERS INITIATED')
-}) 
+// app.get('/users', (req, res)=> {                 // USERS
+//     res.send('USERS INITIATED')
+// }) 
 
-app.get('/parks', (req, res)=> {                 // PARKS
-    res.send('PARKS INITIATED')
-}) 
+// app.get('/parks', (req, res)=> {                 // PARKS
+//     res.send('PARKS INITIATED')
+// }) 
 
-app.get('/finds', (req, res)=> {                 // FINDS
-    res.send('FINDS INITIATED')
-})
+// app.get('/finds', (req, res)=> {                 // FINDS
+//     res.send('FINDS INITIATED')
+// })
 
 
 app.get(
@@ -41,5 +41,31 @@ app.get(
     },
     (req, res) => {
     res.send('RESPONSE SUCCESS')
-}
-)
+});
+
+app.get('/hello', (req, res) => {
+    console.log('IM IN THE HELLO ROUTE')
+    res.send('howdy')
+});
+
+app.post('/hello', (req, res) => {
+    console.log(`POST SUCCESSFULL`)
+    res.send({ msg: 'YOU DID IT!' })
+});
+
+app.get('/message/:id', (req, res) => {
+    res.send({ msg: `MESSAGE #${req.params.id} FOUND`})
+  });
+
+  app.get('/find', (req, res) => {
+    res.send({
+      name: `CALL ME ${req.query.myName} `,
+      age:`I'M ${req.query.myAge} YEARS OLD`
+    })
+  })
+
+// C  get: [Function (anonymous)],
+// R  post: [Function (anonymous)],
+// U  patch: [Functions (anonymous)],   = ALLOWS US TO CHANGE A KEY VALUE PAIR
+//    put: [Function (anonymous)],      = ALLOWS US TO ADD A NEW PAIR IN (EX: IS VACCINATED, IS HYBRID)
+// D  delete: [Function (anonymous)]
